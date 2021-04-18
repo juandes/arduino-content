ae04c929277228d5a4a7567c9164952336e9d6f2
Juan De Dios Santos <me@juandes.com>
2021-04-18 15:11:00 +0200

    Added split

--- python/pyserial/main.py
+++ python/pyserial/main.py
@@ -1,9 +1,11 @@
 import serial
 
 # Use ttyACM0 if it's on the Pi.
-ser = serial.Serial('/dev/cu.usbmodem144301', 9600, 8, 'N', 1, timeout=1)
+# /dev/cu.usbmodem143201
+# /dev/cu.usbmodem144301
+ser = serial.Serial('/dev/cu.usbmodem143201', 9600, 8, 'N', 1, timeout=1)
 
 while True:
     s = ser.readline().decode()
     if s != "":
-        print(s)
+        print(s.split(","))
