f8fe11fa028293ae13cb728b84c47a4031004728
Juan De Dios Santos <me@juandes.com>
2021-04-18 15:44:53 +0200

    Split to int

--- python/pyserial/main.py
+++ python/pyserial/main.py
@@ -1,12 +1,16 @@
 import serial
+from time import time
 
 # Use ttyACM0 if it's on the Pi.
 # /dev/cu.usbmodem143201
 # /dev/cu.usbmodem144301
 # usbmodem144201
-ser = serial.Serial('/dev/cu.usbmodem143201', 9600, 8, 'N', 1, timeout=1)
+ser = serial.Serial('/dev/cu.usbmodem141201', 9600, 8, 'N', 1, timeout=1)
 
 while True:
     s = ser.readline().decode()
     if s != "":
-        print(s.split(","))
+        rows = [float(x) for x in s.split(',')]
+        # Insert local time to list's first position
+        rows.insert(0, int(time()))
+        print(rows)
