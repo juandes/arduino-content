7eb8df5c6daa8502e41c3b29cc4df74396f6faba
Juan De Dios Santos <me@juandes.com>
2021-04-18 15:12:51 +0200

    Update

--- python/pyserial/main.py
+++ python/pyserial/main.py
@@ -1,9 +1,18 @@
 import serial
 
 # Use ttyACM0 if it's on the Pi.
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 # /dev/cu.usbmodem143201
 # /dev/cu.usbmodem144301
+# usbmodem144201
 ser = serial.Serial('/dev/cu.usbmodem143201', 9600, 8, 'N', 1, timeout=1)
+=======
+ser = serial.Serial('/dev/cu.usbmodem144201', 9600, 8, 'N', 1, timeout=1)
+>>>>>>> Stashed changes
+=======
+ser = serial.Serial('/dev/cu.usbmodem144201', 9600, 8, 'N', 1, timeout=1)
+>>>>>>> Stashed changes
 
 while True:
     s = ser.readline().decode()
