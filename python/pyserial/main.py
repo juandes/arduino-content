92068c2c41cf9fc3a1ac93331eabc0c2be5d5d88
Juan De Dios Santos <me@juandes.com>
2021-05-01 17:57:05 +0200

    Added Python script and dataset

--- python/pyserial/main.py
+++ python/pyserial/main.py
@@ -1,11 +1,16 @@
-import serial
+import csv
 from time import time
+
+import serial
 
 # Use ttyACM0 if it's on the Pi.
 # /dev/cu.usbmodem143201
 # /dev/cu.usbmodem144301
 # usbmodem144201
-ser = serial.Serial('/dev/cu.usbmodem141201', 9600, 8, 'N', 1, timeout=1)
+ser = serial.Serial('/dev/cu.usbmodem141301', timeout=1)
+
+f = open("df.csv", "a+")
+writer = csv.writer(f, delimiter=',')
 
 while True:
     s = ser.readline().decode()
@@ -14,3 +19,5 @@ while True:
         # Insert local time to list's first position
         rows.insert(0, int(time()))
         print(rows)
+        writer.writerow(rows)
+        f.flush()
